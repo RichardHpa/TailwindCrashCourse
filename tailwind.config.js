c6855c091f28d6ba4c2f41ca0fa464ea5b47e707
RichardHpa <richard.m.hpa@gmail.com>
2022-06-21 19:54:09 +1200

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    screens: { sm: '480px', md: '768px', lg: '976px', xl: '1440px' },
+    screens: { sm: '480px', md: '768px', lg: '976px', xl: '1200px' },
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
